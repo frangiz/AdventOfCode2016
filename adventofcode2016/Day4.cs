220f145434eb62e6dac3196608d00cace4942aa3
Björn <frangiz@gmail.com>
2016-12-04 17:01:19 +0100

    Day4 fixed.

---
+++ adventofcode2016/Day4.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace adventofcode2016
+{
+	public class Day4
+	{
+		// ---------------------------------------------------------------------------
+		public class Room
+		{
+			private string _roomName;
+			public string RoomName { get { return _roomName; } }
+
+			private int _sectorId;
+			public int SectorId { get { return _sectorId; } }
+
+			private string _checksum;
+			public string Checksum { get { return _checksum; } }
+
+			public Room(string encryptedName)
+			{
+				_roomName = FindRoomName(encryptedName);
+				_sectorId = FindSectorId(encryptedName);
+				_checksum = FindChecksum(encryptedName);
+			}
+
+			private static string FindRoomName(string encryptedName)
+			{
+				var name = "";
+				foreach (var token in encryptedName)
+				{
+					if (char.IsDigit(token)) { break; }
+					name += token;
+				}
+				return name;
+			}
+
+			private static int FindSectorId(string encryptedName)
+			{
+				var sectorId = "";
+				foreach (var token in encryptedName)
+				{
+					if (char.IsDigit(token)) { sectorId += token; }
+					if (token == '[') { break; }
+				}
+				return int.Parse(sectorId);
+			}
+
+			private static string FindChecksum(string encryptedName)
+			{
+				return encryptedName.Substring(encryptedName.IndexOf('[') + 1, 5);
+			}
+
+			private string SuggestedChecksum()
+			{
+				var occurences = FindCharOccurrences(_roomName);
+				var suggestedChecksum = new string(occurences.OrderByDescending(kvp => kvp.Value)
+					.ThenBy(kvp => kvp.Key)
+					.Select(kvp => kvp.Key).ToArray());
+				if (suggestedChecksum.Length > 5) { suggestedChecksum = suggestedChecksum.Substring(0, 5); }
+
+				return suggestedChecksum;
+			}
+
+			public bool IsValidRoom()
+			{
+				return SuggestedChecksum().Equals(_checksum);
+			}
+
+			public void RotateRoomName(int steps)
+			{
+				var newName = "";
+				var charsInAlphabet = 'z' - 'a' + 1;
+				foreach (var token in _roomName)
+				{
+					if (char.IsLetter(token))
+					{
+						int newChar = token + (steps % charsInAlphabet);
+						if (newChar > 'z') { newChar -= charsInAlphabet; }
+						newName += (char)newChar;
+					}
+					else if (token == '-') { newName += " "; }
+				}
+				_roomName = newName.Trim();
+			}
+
+			private static Dictionary<char, int> FindCharOccurrences(string name)
+			{
+				var occurrences = new Dictionary<char, int>();
+				foreach (var token in name)
+				{
+					if (char.IsLetter(token))
+					{
+						if (!occurrences.ContainsKey(token)) { occurrences[token] = 0; }
+						occurrences[token]++;
+					}
+				}
+
+				return occurrences;
+			}
+		}
+
+		// ---------------------------------------------------------------------------
+		public Tuple<int, int> FindSectorId(IEnumerable<string> encryptedNames, bool rotateRoomNames = false)
+		{
+			var sectorIdSum = 0;
+			var northPoleRoom = 0;
+			foreach (var name in encryptedNames)
+			{
+				var room = new Room(name);
+				if (rotateRoomNames)
+				{
+					room.RotateRoomName(room.SectorId);
+					if (room.RoomName.StartsWith("north"))
+					{
+						northPoleRoom = room.SectorId;
+					}
+				}
+				if (room.IsValidRoom())
+				{
+					sectorIdSum += room.SectorId;
+				}
+			}
+			return new Tuple<int, int>(sectorIdSum, northPoleRoom);
+		}
+	}
+}
---
+++ adventofcode2016test/Day4Test.cs
@@ -0,0 +1,99 @@
+﻿using adventofcode2016;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+
+namespace adventofcode2016test
+{
+	[TestFixture]
+	internal class Day4Test
+	{
+		[Test]
+		public void ExampleA1()
+		{
+			var encryptedName = "aaaaa-bbb-z-y-x-123[abxyz]";
+			var room = new Day4.Room(encryptedName);
+
+			Assert.AreEqual("aaaaa-bbb-z-y-x-", room.RoomName);
+			Assert.AreEqual(123, room.SectorId);
+			Assert.AreEqual("abxyz", room.Checksum);
+			Assert.True(room.IsValidRoom());
+		}
+
+		[Test]
+		public void ExampleA2()
+		{
+			var encryptedName = "a-b-c-d-e-f-g-h-987[abcde]";
+			var room = new Day4.Room(encryptedName);
+
+			Assert.AreEqual("a-b-c-d-e-f-g-h-", room.RoomName);
+			Assert.AreEqual(987, room.SectorId);
+			Assert.AreEqual("abcde", room.Checksum);
+			Assert.True(room.IsValidRoom());
+		}
+
+		[Test]
+		public void ExampleA3()
+		{
+			var encryptedName = "not-a-real-room-404[oarel]";
+			var room = new Day4.Room(encryptedName);
+
+			Assert.AreEqual("not-a-real-room-", room.RoomName);
+			Assert.AreEqual(404, room.SectorId);
+			Assert.AreEqual("oarel", room.Checksum);
+			Assert.True(room.IsValidRoom());
+		}
+
+		[Test]
+		public void ExampleA4()
+		{
+			var encryptedName = "totally-real-room-200[decoy]";
+			var room = new Day4.Room(encryptedName);
+
+			Assert.AreEqual("totally-real-room-", room.RoomName);
+			Assert.AreEqual(200, room.SectorId);
+			Assert.AreEqual("decoy", room.Checksum);
+			Assert.False(room.IsValidRoom());
+		}
+
+		[Test]
+		public void ExampleA5()
+		{
+			var encryptedNames = new List<string>
+			{
+				"aaaaa-bbb-z-y-x-123[abxyz]",
+				"a-b-c-d-e-f-g-h-987[abcde]",
+				"not-a-real-room-404[oarel]",
+				"totally-real-room-200[decoy]"
+			};
+
+			Assert.AreEqual(1514, new Day4().FindSectorId(encryptedNames).Item1);
+		}
+
+		[Test]
+		public void AnswerA()
+		{
+			var encryptedNames = File.ReadAllLines("Day4_input.txt");
+
+			Assert.AreEqual(409147, new Day4().FindSectorId(encryptedNames).Item1);
+		}
+
+		[Test]
+		public void ExampleB1()
+		{
+			var encryptedName = "qzmt-zixmtkozy-ivhz-343[abcde]";
+			var room = new Day4.Room(encryptedName);
+			room.RotateRoomName(room.SectorId);
+			Assert.AreEqual("very encrypted name", room.RoomName);
+			Assert.AreEqual(343, room.SectorId);
+		}
+
+		[Test]
+		public void AnswerB()
+		{
+			var encryptedNames = File.ReadAllLines("Day4_input.txt");
+
+			Assert.AreEqual(991, new Day4().FindSectorId(encryptedNames, true).Item2);
+		}
+	}
+}
