95c6cdd2133ededef3d3ddf72ae210fa484fe1a1
bjol <bjorn.p.olsson@saabgroup.com>
2016-12-05 13:55:40 +0100

    Day5 fixed.

---
+++ adventofcode2016/Day5.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace adventofcode2016
+{
+	public class Day5
+	{
+		private long _counter;
+
+		public char[] FindPassword(string doorId, int charsToGet, bool part2 = false)
+		{
+			var password = new char[8];
+			int index = 0;
+			while (index < charsToGet)
+			{
+				var hash = doorId + _counter.ToString();
+				using (var md5 = MD5.Create())
+				{
+					var bytes = md5.ComputeHash(Encoding.ASCII.GetBytes(hash));
+					hash = BitConverter.ToString(bytes).Replace("-", "");
+				}
+				if (hash.StartsWith("00000"))
+				{
+					if (part2)
+					{
+						var i = 0;
+						if (int.TryParse(hash[5].ToString(), out i) &&
+							i < password.Length &&
+							password[i] == '\0')
+						{
+							password[i] = char.ToLower(hash[6]);
+							index++;
+						}
+					}
+					else
+					{
+						password[index++] = char.ToLower(hash[5]);
+					}
+				}
+				_counter++;
+			}
+
+			return password;
+		}
+	}
+}
