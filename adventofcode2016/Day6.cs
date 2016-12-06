48696dd33e794c9bf730f9a34197007b57ff8551
bjol <bjorn.p.olsson@saabgroup.com>
2016-12-06 14:27:18 +0100

    Day6 fixed.

---
+++ adventofcode2016/Day6.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace adventofcode2016
+{
+	public class Day6
+	{
+		private readonly List<Dictionary<char, int>> _data = new List<Dictionary<char, int>>();
+
+		public Day6(int length)
+		{
+			for (var i = 0; i < length; i++)
+			{
+				_data.Add(new Dictionary<char, int>());
+			}
+		}
+
+		public string Decode(IEnumerable<string> lines, bool selectLeastCommonChar = false)
+		{
+			foreach (var line in lines)
+			{
+				AddLine(line);
+			}
+
+			var sb = new StringBuilder();
+			foreach (var tokenInfo in _data)
+			{
+				if (selectLeastCommonChar)
+				{
+					sb.Append(tokenInfo.OrderBy(a => a.Value).ThenByDescending(a => a.Key).First().Key);
+				}
+				else
+				{
+					sb.Append(tokenInfo.OrderByDescending(a => a.Value).ThenBy(a => a.Key).First().Key);
+				}
+			}
+
+			return sb.ToString();
+		}
+
+		private void AddLine(string line)
+		{
+			for (var charIndex = 0; charIndex < line.Length; charIndex++)
+			{
+				var currentChar = line[charIndex];
+				if (!_data[charIndex].ContainsKey(currentChar))
+				{
+					_data[charIndex][currentChar] = 0;
+				}
+				_data[charIndex][currentChar]++;
+			}
+		}
+	}
+}
---
+++ adventofcode2016test/Day6Test.cs
@@ -0,0 +1,75 @@
+﻿using adventofcode2016;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+
+namespace adventofcode2016test
+{
+	[TestFixture]
+	internal class Day6Test
+	{
+		[Test]
+		public void ExampleA1()
+		{
+			var lines = new List<string>
+			{
+				"eedadn",
+				"drvtee",
+				"eandsr",
+				"raavrd",
+				"atevrs",
+				"tsrnev",
+				"sdttsa",
+				"rasrtv",
+				"nssdts",
+				"ntnada",
+				"svetve",
+				"tesnvt",
+				"vntsnd",
+				"vrdear",
+				"dvrsen",
+				"enarar"
+			};
+			Assert.AreEqual("easter", new Day6(lines[0].Length).Decode(lines));
+		}
+
+		[Test]
+		public void AnswerA()
+		{
+			var lines = File.ReadAllLines("Day6_input.txt");
+			Assert.AreEqual("qqqluigu", new Day6(lines[0].Length).Decode(lines));
+		}
+
+		[Test]
+		public void ExampleB1()
+		{
+			var lines = new List<string>
+			{
+				"eedadn",
+				"drvtee",
+				"eandsr",
+				"raavrd",
+				"atevrs",
+				"tsrnev",
+				"sdttsa",
+				"rasrtv",
+				"nssdts",
+				"ntnada",
+				"svetve",
+				"tesnvt",
+				"vntsnd",
+				"vrdear",
+				"dvrsen",
+				"enarar"
+			};
+			Assert.AreEqual("advent", new Day6(lines[0].Length).Decode(lines, true));
+		}
+
+		[Test]
+		public void AnswerB()
+		{
+			var lines = File.ReadAllLines("Day6_input.txt");
+			Assert.AreEqual("lsoypmia", new Day6(lines[0].Length).Decode(lines, true));
+		}
+	}
+}
