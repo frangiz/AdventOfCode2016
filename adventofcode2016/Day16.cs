40a417a08e1928540a6f851e24621cf1bc7ce361
Björn <frangiz@gmail.com>
2016-12-17 18:14:26 +0100

    Day16 fixed.

---
+++ adventofcode2016/Day16.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace adventofcode2016
+{
+	public class Day16 : IDay
+	{
+		public static string GenerateData(string initialState, int length)
+		{
+			var data = initialState;
+			while (data.Length < length)
+			{
+				data += "0" +new string(data.Reverse().Select(c => c == '1' ? '0' : '1').ToArray());
+			}
+
+			return data.Substring(0, length);
+		}
+
+		public static string GenerateChecksum(string data)
+		{
+			var sb = new StringBuilder();
+			for (int i = 0; i < data.Length; i+=2)
+			{
+				sb.Append(data[i] == data[i+1] ? '1' : '0');
+			}
+
+			return sb.ToString().Length % 2 == 0 ? GenerateChecksum(sb.ToString()) : sb.ToString();
+		}
+
+		// ---------------------------------------------------------------------------
+		public void PrintDay()
+		{
+			Console.WriteLine("--- Day 16: Dragon Checksum ---");
+			{
+				var data = GenerateData("01000100010010111", 272);
+				Console.WriteLine("Answer A: " + GenerateChecksum(data));
+			}
+			{
+				var data = GenerateData("01000100010010111", 35651584);
+				Console.WriteLine("Answer B: " + GenerateChecksum(data));
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ adventofcode2016test/Day16Test.cs
@@ -0,0 +1,26 @@
+﻿using adventofcode2016;
+using NUnit.Framework;
+
+namespace adventofcode2016test
+{
+	[TestFixture]
+	internal class Day16Test
+	{
+		[Test]
+		public void ExampleA1()
+		{
+			var data = Day16.GenerateData("110010110100", 12);
+			Assert.AreEqual("110010110100", data);
+			Assert.AreEqual("100", Day16.GenerateChecksum(data));
+		}
+
+		[Test]
+		public void ExampleA2()
+		{
+			var data = Day16.GenerateData("10000", 20);
+			Assert.AreEqual("10000011110010000111", data);
+			Assert.AreEqual("01100", Day16.GenerateChecksum(data));
+		}
+
+	}
+}
