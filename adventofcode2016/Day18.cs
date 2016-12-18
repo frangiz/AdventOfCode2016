4e86e1984cf57253761d332dfda60112e146b69a
Björn <frangiz@gmail.com>
2016-12-18 23:01:56 +0100

    Day18 fixed.

---
+++ adventofcode2016/Day18.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace adventofcode2016
+{
+	public class Day18 : IDay
+	{
+		public class TrapRoom
+		{
+			public int NumberOfSafeTiles { get { return _tiles.Count(t => t == TileType.SafeTile); } }
+
+			private enum TileType { SafeTile, Trap };
+			private List<TileType> _tiles;
+			private static int width;
+
+			public TrapRoom(string firstRow)
+			{
+				_tiles = new List<TileType>();
+				foreach (var tile in firstRow)
+				{
+					_tiles.Add(tile == '.' ? TileType.SafeTile : TileType.Trap);
+				}
+				width = _tiles.Count;
+			}
+
+			public void AddRows(int rows)
+			{
+				for (int i = 0; i < rows; i++)
+				{
+					// First tile in a row is a bit special
+					var preRowCenterIndex = _tiles.Count - width;
+					AddTile(TileType.SafeTile, _tiles[preRowCenterIndex], _tiles[preRowCenterIndex + 1]);
+					preRowCenterIndex++;
+					for (int j = 1; j < width - 1; j++)
+					{
+						AddTile(_tiles[preRowCenterIndex - 1], _tiles[preRowCenterIndex], _tiles[preRowCenterIndex + 1]);
+						preRowCenterIndex++;
+					}
+					// Last tile in a row is a bit special
+					AddTile(_tiles[preRowCenterIndex - 1], _tiles[preRowCenterIndex], TileType.SafeTile);
+				}
+			}
+
+			private void AddTile(TileType left, TileType center, TileType right)
+			{
+				if (left == TileType.Trap && center == TileType.Trap && right != TileType.Trap)
+				{
+					_tiles.Add(TileType.Trap);
+				}
+				else if (left != TileType.Trap && center == TileType.Trap && right == TileType.Trap)
+				{
+					_tiles.Add(TileType.Trap);
+				}
+				else if (left == TileType.Trap && center != TileType.Trap && right != TileType.Trap)
+				{
+					_tiles.Add(TileType.Trap);
+				}
+				else if (left != TileType.Trap && center != TileType.Trap && right == TileType.Trap)
+				{
+					_tiles.Add(TileType.Trap);
+				}
+				else
+				{
+					_tiles.Add(TileType.SafeTile);
+				}
+			}
+		}
+
+		// ---------------------------------------------------------------------------
+		public void PrintDay()
+		{
+			Console.WriteLine("--- Day 18: Like a Rogue ---");
+			{
+				var trapRoom = new TrapRoom(File.ReadAllText("Day18_input.txt").Trim());
+				trapRoom.AddRows(39);
+				Console.WriteLine("Answer A: " + trapRoom.NumberOfSafeTiles);
+			}
+			{
+				var trapRoom = new TrapRoom(File.ReadAllText("Day18_input.txt").Trim());
+				trapRoom.AddRows(399999);
+				Console.WriteLine("Answer B: " + trapRoom.NumberOfSafeTiles);
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ adventofcode2016test/Day18Test.cs
@@ -0,0 +1,18 @@
+﻿using adventofcode2016;
+using NUnit.Framework;
+
+namespace adventofcode2016test
+{
+	[TestFixture]
+	internal class Day18Test
+	{
+		[Test]
+		public void ExampleA1()
+		{
+			var trapRoom = new Day18.TrapRoom(".^^.^.^^^^");
+			trapRoom.AddRows(9);
+			Assert.AreEqual(38, trapRoom.NumberOfSafeTiles);
+		}
+
+	}
+}
