e87c05744869dda2070ef1ad5c59f42384e5eec8
Björn <frangiz@gmail.com>
2016-12-20 21:47:13 +0100

    Day20 fixed.

---
+++ adventofcode2016/Day20.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace adventofcode2016
+{
+	public class Day20 : IDay
+	{
+		public class Firewall
+		{
+			private readonly List<Tuple<ulong, ulong>> _blockedRanges;
+
+			public Firewall(IEnumerable<string> blockedRanges)
+			{
+				_blockedRanges = new List<Tuple<ulong, ulong>>();
+				foreach (var range in blockedRanges)
+				{
+					var parts = range.Split('-');
+					_blockedRanges.Add(new Tuple<ulong, ulong>(ulong.Parse(parts[0]), ulong.Parse(parts[1])));
+				}
+			}
+
+			public ulong LowestAvailableIP(ulong min, ulong max)
+			{
+				var current = min;
+				while (current <= max)
+				{
+					var blocked = _blockedRanges.FirstOrDefault(r => r.Item1 <= current && current <= r.Item2);
+					if (blocked == null)
+					{
+						return current;
+					}
+					current = blocked.Item2 + 1;
+				}
+
+				return current;
+			}
+
+			public ulong NumberOfAllowedIps(ulong min, ulong max)
+			{
+				ulong counter = 0;
+				var current = min;
+
+				while (current <= max)
+				{
+					var blocked = _blockedRanges.FirstOrDefault(r => r.Item1 <= current && current <= r.Item2);
+					if (blocked == null)
+					{
+						counter++;
+						current++;
+					}
+					else
+					{
+						current = blocked.Item2 + 1;
+					}
+				}
+
+				return counter;
+			}
+		}
+
+		// ---------------------------------------------------------------------------
+		public void PrintDay()
+		{
+			Console.WriteLine("--- Day 20: Firewall Rules ---");
+			{
+				var fw = new Firewall(File.ReadAllLines("DAy20_input.txt"));
+				Console.WriteLine("Answer A: " + fw.LowestAvailableIP(0, ulong.MaxValue));
+			}
+			{
+				var fw = new Firewall(File.ReadAllLines("DAy20_input.txt"));
+				Console.WriteLine("Answer B: " + fw.NumberOfAllowedIps(0, uint.MaxValue));
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ adventofcode2016test/Day20Test.cs
@@ -0,0 +1,23 @@
+﻿using adventofcode2016;
+using NUnit.Framework;
+
+namespace adventofcode2016test
+{
+	[TestFixture]
+	internal class Day20Test
+	{
+		[Test]
+		public void ExampleA1()
+		{
+			var fw = new Day20.Firewall(new[] { "5-8", "0-2", "4-7" });
+			Assert.AreEqual(3, fw.LowestAvailableIP(0, 9));
+		}
+
+		[Test]
+		public void ExampleB1()
+		{
+			var fw = new Day20.Firewall(new[] { "5-8", "0-2", "4-7" });
+			Assert.AreEqual(2, fw.NumberOfAllowedIps(0, 9));
+		}
+	}
+}
