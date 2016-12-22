6e87b0fd97388c6f0f06d67c3d8b442aefda354c
Björn <frangiz@gmail.com>
2016-12-22 22:23:29 +0100

    Day21 fixed.

---
+++ adventofcode2016/Day21.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace adventofcode2016
+{
+	public class Day21 : IDay
+	{
+		public class Scrambler
+		{
+			// -------------------------------------------------------------------------
+			private interface ICommand { void Execute(ref char[] password); }
+			private class NopCommand : ICommand { public void Execute(ref char[] password) { } }
+
+			private Regex _swapPositionRegex = new Regex("swap position (\\d+) with position (\\d+)");
+			private class SwapPositionCmd : ICommand
+			{
+				public int X { get; set; }
+				public int Y { get; set; }
+				public void Execute(ref char[] password)
+				{
+					var tmp = password[X]; password[X] = password[Y]; password[Y] = tmp;
+				}
+			}
+
+			private Regex _swapCharRegex = new Regex("swap letter (\\w) with letter (\\w)");
+			private class SwapCharCmd : ICommand
+			{
+				public char X { get; set; }
+				public char Y { get; set; }
+				public void Execute(ref char[] password)
+				{
+					var pos = Array.IndexOf(password, X);
+					password[Array.IndexOf(password, Y)] = X;
+					password[pos] = Y;
+				}
+			}
+
+			private Regex _rotateStepsRegex = new Regex("rotate (left|right) (\\d+) step");
+			private class RotateStepsCmd : ICommand
+			{
+				public enum Directions { Left, Right };
+				public Directions Direction { get; set; }
+				public int Steps { get; set; }
+				public void Execute(ref char[] password)
+				{
+					var newIndexFor0 = (Steps * (Direction == Directions.Left ? 1 : -1)) % password.Length;
+					if (newIndexFor0 < 0) { newIndexFor0 += password.Length; }
+					var tmp = new char[password.Length];
+					Array.Copy(password, newIndexFor0, tmp, 0, password.Length - newIndexFor0);
+					Array.Copy(password, 0, tmp, password.Length - newIndexFor0, newIndexFor0);
+
+					password = tmp;
+				}
+			}
+
+			private Regex _rotatePositionRegex = new Regex("rotate based on position of letter (\\w)");
+			private class RotatePositionCmd : ICommand
+			{
+				public char Letter { get; set; }
+				public bool Reversed { get; set; }
+				public void Execute(ref char[] password)
+				{
+					var index = Array.IndexOf(password, Letter);
+					var cmd = new RotateStepsCmd
+					{
+						Direction = RotateStepsCmd.Directions.Right,
+						Steps = index + 1 + (index >= 4 ? 1 : 0)
+					};
+					if (Reversed)
+					{
+						cmd.Direction = RotateStepsCmd.Directions.Left;
+						for (int i = 0; i < password.Length; i++)
+						{
+							var steps = i + 1 + (i >= 4 ? 1 : 0);
+							if ((i + steps) % password.Length == index)
+							{
+								cmd.Steps = steps;
+								break;
+							}
+						}
+					}
+
+					cmd.Execute(ref password);
+				}
+			}
+
+			private Regex _reversePositionsRegex = new Regex("reverse positions (\\d+) through (\\d+)");
+			private class ReversePositionsCmd : ICommand
+			{
+				public int Start { get; set; }
+				public int End { get; set; }
+				public void Execute(ref char[] password)
+				{
+					Array.Reverse(password, Start, End - Start + 1);
+				}
+			}
+
+			private Regex _moveCharRegex = new Regex("move position (\\d+) to position (\\d+)");
+			private class MoveCharCmd : ICommand
+			{
+				public int From { get; set; }
+				public int To { get; set; }
+				public void Execute(ref char[] password)
+				{
+					var startVal = password[From];
+					if (From < To)
+					{
+						Array.Copy(password, From + 1, password, From, To - From);
+					}
+					else
+					{
+						Array.Copy(password, To, password, To + 1, From - To);
+					}
+					password[To] = startVal;
+				}
+			}
+
+			// -------------------------------------------------------------------------
+			private char[] _password;
+
+			public string Scramble(string password, IEnumerable<string> instructions)
+			{
+				_password = password.ToArray();
+
+				foreach (var instruction in instructions)
+				{
+					var cmd = ParseCommand(instruction);
+					cmd.Execute(ref _password);
+				}
+
+				return new string(_password);
+			}
+
+			public string Unscramble(string password, IEnumerable<string> instructions)
+			{
+				_password = password.ToArray();
+
+				foreach (var instruction in instructions.Reverse())
+				{
+					var cmd = ParseCommand(instruction, true);
+					cmd.Execute(ref _password);
+				}
+
+				return new string(_password);
+			}
+
+			private ICommand ParseCommand(string instructions, bool reversed = false)
+			{
+				var match = _swapPositionRegex.Match(instructions);
+				if (match.Success)
+				{
+					return new SwapPositionCmd
+					{
+						X = int.Parse(match.Groups[1].Value),
+						Y = int.Parse(match.Groups[2].Value)
+					};
+				}
+
+				match = _swapCharRegex.Match(instructions);
+				if (match.Success)
+				{
+					return new SwapCharCmd
+					{
+						X = match.Groups[1].Value[0],
+						Y = match.Groups[2].Value[0]
+					};
+				}
+
+				match = _rotateStepsRegex.Match(instructions);
+				if (match.Success)
+				{
+					var dir = match.Groups[1].Value.ToLower() == "left" ?
+							RotateStepsCmd.Directions.Left : RotateStepsCmd.Directions.Right;
+					if (reversed)
+					{
+						dir = dir == RotateStepsCmd.Directions.Left ?
+							RotateStepsCmd.Directions.Right : RotateStepsCmd.Directions.Left;
+					}
+					return new RotateStepsCmd
+					{
+						Direction = dir,
+						Steps = int.Parse(match.Groups[2].Value)
+					};
+				}
+
+				match = _rotatePositionRegex.Match(instructions);
+				if (match.Success)
+				{
+					return new RotatePositionCmd
+					{
+						Letter = match.Groups[1].Value[0],
+						Reversed = reversed
+					};
+				}
+
+				match = _reversePositionsRegex.Match(instructions);
+				if (match.Success)
+				{
+					return new ReversePositionsCmd
+					{
+						Start = int.Parse(match.Groups[1].Value),
+						End = int.Parse(match.Groups[2].Value)
+					};
+				}
+
+				match = _moveCharRegex.Match(instructions);
+				if (match.Success)
+				{
+					return new MoveCharCmd
+					{
+						From = int.Parse(match.Groups[reversed ? 2 : 1].Value),
+						To = int.Parse(match.Groups[reversed ? 1 : 2].Value)
+					};
+				}
+
+				return new NopCommand();
+			}
+		}
+
+		// ---------------------------------------------------------------------------
+		public void PrintDay()
+		{
+			Console.WriteLine("--- Day 21: Scrambled Letters and Hash ---");
+			{
+				var scrambler = new Scrambler();
+				Console.WriteLine("Answer A: " + scrambler.Scramble("abcdefgh",
+					File.ReadAllLines("Day21_input.txt")));
+			}
+			{
+				var scrambler = new Scrambler();
+				Console.WriteLine("Answer A: " + scrambler.Unscramble("fbgdceah",
+					File.ReadAllLines("Day21_input.txt")));
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ adventofcode2016test/Day21Test.cs
@@ -0,0 +1,220 @@
+﻿using adventofcode2016;
+using NUnit.Framework;
+using System;
+
+namespace adventofcode2016test
+{
+	[TestFixture]
+	internal class Day21Test
+	{
+		[Test]
+		public void ExampleA1()
+		{
+			var scrambler = new Day21.Scrambler();
+			Assert.AreEqual("ebcda", scrambler.Scramble("abcde", new[]
+			{
+				"swap position 4 with position 0"
+			}));
+		}
+
+		[Test]
+		public void ExampleA2()
+		{
+			var scrambler = new Day21.Scrambler();
+			Assert.AreEqual("edcba", scrambler.Scramble("abcde", new[]
+			{
+				"swap position 4 with position 0",
+				"swap letter d with letter b"
+			}));
+		}
+
+		[Test]
+		public void ExampleA3()
+		{
+			var scrambler = new Day21.Scrambler();
+			Assert.AreEqual("abcde", scrambler.Scramble("abcde", new[]
+			{
+				"swap position 4 with position 0",
+				"swap letter d with letter b",
+				"reverse positions 0 through 4"
+			}));
+		}
+
+		[Test]
+		public void ExampleA4()
+		{
+			var scrambler = new Day21.Scrambler();
+			Assert.AreEqual("bcdea", scrambler.Scramble("abcde", new[]
+			{
+				"swap position 4 with position 0",
+				"swap letter d with letter b",
+				"reverse positions 0 through 4",
+				"rotate left 1 step"
+			}));
+		}
+
+		[Test]
+		public void ExampleA5_1()
+		{
+			var scrambler = new Day21.Scrambler();
+			Assert.AreEqual("bdeac", scrambler.Scramble("abcde", new[]
+			{
+				"swap position 4 with position 0",
+				"swap letter d with letter b",
+				"reverse positions 0 through 4",
+				"rotate left 1 step",
+				"move position 1 to position 4"
+			}));
+		}
+
+		[Test]
+		public void ExampleA5_2()
+		{
+			var scrambler = new Day21.Scrambler();
+			Assert.AreEqual("bacde", scrambler.Scramble("abcde", new[]
+			{
+				"swap position 4 with position 0",
+				"swap letter d with letter b",
+				"reverse positions 0 through 4",
+				"rotate left 1 step",
+				"move position 4 to position 1"
+			}));
+		}
+
+		[Test]
+		public void ExampleA6()
+		{
+			var scrambler = new Day21.Scrambler();
+			Assert.AreEqual("abdec", scrambler.Scramble("abcde", new[]
+			{
+				"swap position 4 with position 0",
+				"swap letter d with letter b",
+				"reverse positions 0 through 4",
+				"rotate left 1 step",
+				"move position 1 to position 4",
+				"move position 3 to position 0"
+			}));
+		}
+
+		[Test]
+		public void ExampleA7()
+		{
+			var scrambler = new Day21.Scrambler();
+			Assert.AreEqual("ecabd", scrambler.Scramble("abcde", new[]
+			{
+				"swap position 4 with position 0",
+				"swap letter d with letter b",
+				"reverse positions 0 through 4",
+				"rotate left 1 step",
+				"move position 1 to position 4",
+				"move position 3 to position 0",
+				"rotate based on position of letter b"
+			}));
+		}
+
+		[Test]
+		public void ExampleA8()
+		{
+			var scrambler = new Day21.Scrambler();
+			Assert.AreEqual("decab", scrambler.Scramble("abcde", new[]
+			{
+				"swap position 4 with position 0",
+				"swap letter d with letter b",
+				"reverse positions 0 through 4",
+				"rotate left 1 step",
+				"move position 1 to position 4",
+				"move position 3 to position 0",
+				"rotate based on position of letter b",
+				"rotate based on position of letter d"
+			}));
+		}
+
+		[Test]
+		public void ExampleB1()
+		{
+			var scrambler = new Day21.Scrambler();
+			var instructions = new[]
+			{
+				"rotate based on position of letter d"
+			};
+			Assert.AreEqual("d_______", scrambler.Unscramble("_d______", instructions));
+		}
+
+		[Test]
+		public void ExampleB2()
+		{
+			var scrambler = new Day21.Scrambler();
+			var instructions = new[]
+			{
+				"rotate based on position of letter d"
+			};
+			Assert.AreEqual("_d______", scrambler.Unscramble("___d____", instructions));
+		}
+
+		[Test]
+		public void ExampleB3()
+		{
+			var scrambler = new Day21.Scrambler();
+			var instructions = new[]
+			{
+				"rotate based on position of letter d"
+			};
+			Assert.AreEqual("__d_____", scrambler.Unscramble("_____d__", instructions));
+		}
+
+		[Test]
+		public void ExampleB4()
+		{
+			var scrambler = new Day21.Scrambler();
+			var instructions = new[]
+			{
+				"rotate based on position of letter d"
+			};
+			Assert.AreEqual("___d____", scrambler.Unscramble("_______d", instructions));
+		}
+
+		[Test]
+		public void ExampleB5()
+		{
+			var scrambler = new Day21.Scrambler();
+			var instructions = new[]
+			{
+				"rotate based on position of letter d"
+			};
+			Assert.AreEqual("____d___", scrambler.Unscramble("__d_____", instructions));
+		}
+
+		[Test]
+		public void ExampleB6()
+		{
+			var scrambler = new Day21.Scrambler();
+			var instructions = new[]
+			{
+				"rotate based on position of letter d"
+			};
+			Assert.AreEqual("_____d__", scrambler.Unscramble("____d___", instructions));
+		}
+
+		[Test]
+		public void ExampleB7()
+		{
+			var scrambler = new Day21.Scrambler();
+			var instructions = new[]
+			{
+				"rotate based on position of letter d"
+			};
+			Assert.AreEqual("______d_", scrambler.Unscramble("______d_", instructions));
+		}
+
+		[Test]
+		public void ExampleB8()
+		{
+			var scrambler = new Day21.Scrambler();
+			var instructions = new[]
+			{
+				"rotate based on position of letter d"
+			};
+			Assert.AreEqual("_______d", scrambler.Unscramble("d_______", instructions));
+		}
+	}
+}
